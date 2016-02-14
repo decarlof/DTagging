90e183df8c9930fe16acdf7cbe631530083d363c
Francesco De Carlo <decarlof@gmail.com>
2016-02-13 22:41:18 -0600

    tested

--- pv_daemon.py
+++ pv_daemon.py
@@ -12,7 +12,7 @@ import sys
 
 # set ~/scheduling.ini to match your configuration
 import dmagic.scheduling as sch
-import epics_pv_32ID as pv
+import pv_32ID as pv
 
 def pv_daemon():
     # set the experiment date 
@@ -22,15 +22,15 @@ def pv_daemon():
     pv.time_iso.put(now.isoformat())
 
     # get PI information
-    user_name, user_institution, user_badge, user_email = sch.find_user_info(now)
+    user_name, user_institution, user_badge, user_email = sch.find_pi_info(now)
     pv.user_name.put(user_name)
-    pv.user_institution.put(user_institution)
-    pv.user_badge.put(user_badge)
+    pv.user_affiliation.put(user_institution)
     pv.user_email.put(user_email)
+    pv.user_badge.put(user_badge)
 
     # get experiment information
-    proposal_id, proposal_title = sch.find_experiment_info(now)
-    pv.proposal_number.put(proposal_id)
+    proposal_number, proposal_title = sch.find_experiment_info(now)
+    pv.proposal_number.put(proposal_number)
     pv.proposal_title.put(proposal_title)
 
 try:
